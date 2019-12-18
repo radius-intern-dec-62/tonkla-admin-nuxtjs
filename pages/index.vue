c0c43d06043a34a90b8bd410f4cb2f622cc51d54
Supadech <supadech.dw@gmail.com>
2019-12-18 11:51:12 +0700

    :recycle:

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,9 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        tonkla-admin
-      </h1>
-      <h2 class="subtitle">
-        Tonkla admin by nuxtjs
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </section>
+  <section class="container"></section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+export default {}
 </script>
 
 <style>
@@ -65,9 +37,5 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
